e8a790479cccd22a2ec7d4e230fb3eebb22c6b45
Muzahidul Islam <muzahidulislam01226@gmail.com>
2023-05-13 15:00:30 +0530

    data ingestion done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,75 +1,69 @@
-import os
-import sys
-from dataclasses import dataclass
-
-from sklearn.linear_model import LinearRegression
+# Basic Import
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object,evaluate_models
+from src.utils import save_object
+from src.utils import evaluate_model
 
-@dataclass
+from dataclasses import dataclass
+import sys
+import os
+
+@dataclass 
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-
-    def initiate_model_trainer(self,train_array,test_array):
+    def initate_model_training(self,train_array,test_array):
         try:
-            logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test=(
+            logging.info('Splitting Dependent and Independent variables from train and test data')
+            X_train, y_train, X_test, y_test = (
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-            models = {'Linear Regression': LinearRegression(),
-          'KNN': KNeighborsRegressor(weights='distance'),
-          'Decision Tree': DecisionTreeRegressor(),
-          'Random Forest': RandomForestRegressor()}
-            params = {'Linear Regression': {'normalize': [True, False]},
-          'KNN': {'n_neighbors': [3, 5, 7]},
-          'Decision Tree': {'criterion': ['mse', 'friedman_mse'], 'max_depth': [3, 5, 7]},
-          'Random Forest': {'n_estimators': [10, 50, 100], 'max_features': ['sqrt', 'log2']}}
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                              models=models,param=params)
+            models={
+            'LinearRegression':LinearRegression(),
+            'Lasso':Lasso(),
+            'Ridge':Ridge(),
+            'Elasticnet':ElasticNet(),
+            'DecisionTree':DecisionTreeRegressor()
+        }
             
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            print(model_report)
+            print('\n====================================================================================\n')
+            logging.info(f'Model Report : {model_report}')
 
-            ## To get best model name from dict
+            # To get best model score from dictionary 
+            best_model_score = max(sorted(model_report.values()))
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+            
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            print(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
+            print('\n====================================================================================\n')
+            logging.info(f'Best Model Found , Model Name : {best_model_name} , R2 Score : {best_model_score}')
 
             save_object(
-                file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model
+                 file_path=self.model_trainer_config.trained_model_file_path,
+                 obj=best_model
             )
+          
 
-            predicted=best_model.predict(X_test)
-
-            Accuracy_score = r2_score(y_test, predicted)
-            return Accuracy_score
-            
-
-
-
-            
         except Exception as e:
+            logging.info('Exception occured at Model Training')
             raise CustomException(e,sys)
